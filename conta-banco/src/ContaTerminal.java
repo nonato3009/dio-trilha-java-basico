dfb32dfe9fedc0e53cccc682cf9777f893c63335
nonato3009 <nonato3@hotmail.com>
2023-11-14 17:29:00 -0300

    desfio conta terminal java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
+        //Conhecer e importar a classe Scanner
+            Scanner sc = new Scanner(System.in);
+        //Exibir as mensagens para o nosso usuario
+            System.out.print("Digite o numero da agência: ");
+            String agencia = sc.nextLine();
+            System.out.print("Digite o número da conta: ");
+            int numero = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Digite o nome do cliente: ");
+            String nome = sc.nextLine();
+            System.out.print("Por favor, digite o saldo da conta: ");
+            double saldo = sc.nextDouble();
+
+            System.out.println();
+            
+        //Obter pela classe Scanner os valores digitados no terminal
+            String mensagem = "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+             + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+
+        //Exibir a mensgem da conta criada 
+              System.out.println(mensagem);
+
+        sc.close();
     }
 }
